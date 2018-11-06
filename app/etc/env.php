afbe6ebe17040c76357a5589821c2f0ac9da9161
Kazi Hasan Ali <kazhasanali@gmail.com>
2018-11-07 04:01:09 +0530

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1uk43s'
+    ],
+    'crypt' => [
+        'key' => 'cc751483064ffbe2c2534fa0b00650f8'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'translate' => 0,
+        'config_webservice' => 0
+    ],
+    'install' => [
+        'date' => 'Tue, 28 Aug 2018 18:12:11 +0000'
+    ]
+];
